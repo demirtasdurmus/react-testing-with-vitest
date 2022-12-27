dbdd550c7556d7e0501eebed04163d0ffd84c718
Durmuş <demirtasdurmus@gmail.com>
2022-12-27 14:54:19 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: "./vitest.setup.ts",
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: [
+        'node_modules/',
+        './vitest.setup.ts',
+      ],
+    },
+  }
+})
